63db9f8c35f489dcfdc0c2a2021b5193a88337fe
C2linaung <c2linaung.main@gmail.com>
2024-10-02 18:50:37 +0800

    changed name of main and test files

--- src/main/java/seedu/planpal/PlanPal.java
+++ src/main/java/seedu/planpal/PlanPal.java
@@ -1,17 +1,17 @@
-package seedu.duke;
+package seedu.planpal;
 
 import java.util.Scanner;
 
-public class Duke {
+public class PlanPal {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
 
