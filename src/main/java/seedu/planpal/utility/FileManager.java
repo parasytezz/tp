972db4c8698b81940d8d6bf4d58c3ae7dfd55e95
anlamm <anlammaaa@gmail.com>
2024-10-08 00:12:25 +0800

    Debug and comment in FileManager

--- src/main/java/seedu/planpal/utility/FileManager.java
+++ src/main/java/seedu/planpal/utility/FileManager.java
@@ -1,12 +1,14 @@
 package seedu.planpal.utility;
 
-import seedu.planpal.contacts.ContactManager;
+import seedu.planpal.exceptions.EmptyDescriptionException;
 import seedu.planpal.exceptions.PlanPalExceptions;
 
 import java.io.*;
-import java.text.ParseException;
 import java.util.Scanner;
 
+/**
+ * Manages the loading and saving of contacts within the PlanPal application.
+ */
 public class FileManager {
     private static final String directoryName = "data";
     private static final String fileName = "contacts.txt";
@@ -14,6 +16,12 @@ public class FileManager {
     private static final String DELETE_COMMAND = "delete";
     private static final String EDIT_COMMAND = "edit";
 
+    /**
+     * Constructor of FileManager
+     * Load commands from stored data if exists
+     *
+     * @param parser Parser to load the stored data
+     */
     public FileManager(Parser parser) {
         System.out.println("Loading data from file...\n");
         Scanner scanner = null;
@@ -30,12 +38,24 @@ public class FileManager {
         }
     }
 
+    /**
+     * Helper function to load one line of command from the stored data
+     *
+     * @param line data stored in a line
+     * @param parser Parser to load the stored data
+     */
     private void loadData(String line, Parser parser) {
         try {
             parser.processCommand(line, null);
         } catch (PlanPalExceptions ignore) {}
     }
 
+    /**
+     * Save command to the stored data
+     *
+     * @param commandType type of command to be stored
+     * @param description the description of the contacts to find. This must not be empty.
+     */
     public void save(String commandType, String description) {
         FileWriter fw;
         BufferedWriter bw;
