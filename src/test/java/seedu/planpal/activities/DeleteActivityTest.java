5c7a930231c5017a320fd8fa6ab0e1bc6669030a
DESKTOP-TJ2T7O3\Dell <e1122313@u.nus.edu>
2024-11-02 22:40:04 +0800

    add JUnit test for Edit, Find, and View activity

---
+++ src/test/java/seedu/planpal/activities/DeleteActivityTest.java
@@ -0,0 +1,4 @@
+package seedu.planpal.activities;
+
+public class DeleteActivityTest {
+}
---
+++ src/test/java/seedu/planpal/activities/ViewActivityListTest.java
@@ -0,0 +1,35 @@
+package seedu.planpal.activities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.planpal.exceptions.PlanPalExceptions;
+import seedu.planpal.modes.activities.ActivityManager;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ViewActivityListTest {
+    private static final ByteArrayOutputStream OUTPUT_STREAM = new ByteArrayOutputStream();
+    private ActivityManager activityManager;
+
+    @BeforeEach
+    public void setUp() {
+        activityManager = new ActivityManager();
+        System.setOut(new PrintStream(OUTPUT_STREAM));
+        OUTPUT_STREAM.reset();
+    }
+
+    @Test
+    public void viewActivityList_success() {
+        try {
+            activityManager.addActivity("/name: swimming /activityType: exercise");
+            activityManager.viewActivityList();
+            assertEquals("[activity = swimming, activity type = exercise]", activityManager.getActivityList().get(0).toString());
+        } catch (PlanPalExceptions e) {
+            fail(e.getMessage());
+        }
+    }
+}
