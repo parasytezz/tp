609db550f90186865e64a3d5fb7595cc5f9eba0c
Yisoong <yisoong23@gmail.com>
2024-11-06 22:24:51 +0800

    Handle list case for month with no expenses and add JUnit tests

--- src/test/java/seedu/planpal/expenses/ViewExpenseListTest.java
+++ src/test/java/seedu/planpal/expenses/ViewExpenseListTest.java
@@ -3,6 +3,7 @@ package seedu.planpal.expenses;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.planpal.exceptions.PlanPalExceptions;
+import seedu.planpal.exceptions.expenses.NoBudgetException;
 import seedu.planpal.modes.expenses.managers.ExpenseManager;
 
 import java.io.ByteArrayOutputStream;
@@ -11,6 +12,7 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class ViewExpenseListTest {
     private static final ByteArrayOutputStream OUTPUT_STREAM = new ByteArrayOutputStream();
@@ -48,6 +50,45 @@ public class ViewExpenseListTest {
             double totalCost = expenseManager.getTotalCost(expenseManager.getMonthlyExpenses());
             assertEquals(600.0, totalCost);
             assertTrue(OUTPUT_STREAM.toString().contains("It's time to readjust your spending habits!"));
+        } catch (PlanPalExceptions e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void noBudget_throwNoBudgetException() {
+        try {
+            expenseManager.viewExpenseList("/month:2024-03");
+            fail("Expected NoBudgetException to be thrown");
+        } catch (PlanPalExceptions e) {
+            assertInstanceOf(NoBudgetException.class, e);
+        }
+    }
+
+    @Test
+    public void validBudgetAndExpenses_displayCorrectly() {
+        try {
+            expenseManager.getBudgetManager().setBudget("1000");
+            expenseManager.addExpense("/name:trial1 /cost:200");
+            expenseManager.addExpense("/name:trial2 /cost:300");
+            expenseManager.viewExpenseList();
+
+            double totalCost = expenseManager.getTotalCost(expenseManager.getMonthlyExpenses());
+            assertEquals(500.0, totalCost);
+            assertTrue(OUTPUT_STREAM.toString().contains("Remaining budget: $500.0"));
+        } catch (PlanPalExceptions e) {
+            fail(e.getMessage());
+        }
+    }
+
+
+    @Test
+    public void noExpenses_displayNoExpensesMessage() {
+        try {
+            expenseManager.getBudgetManager().setBudget("1000 /month:2024-02");
+            expenseManager.viewExpenseList("/month:2024-02");
+
+            assertTrue(OUTPUT_STREAM.toString().contains("No Expenses"));
         } catch (PlanPalExceptions e) {
             fail(e.getMessage());
         }
