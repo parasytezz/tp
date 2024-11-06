42c8fb01e575b83a8b2d51008fcba7284bd393e6
C2linaung <c2linaung.main@gmail.com>
2024-11-07 01:39:21 +0800

    complete added test for recurring. left find and delete

--- src/test/java/seedu/planpal/expenses/ViewExpenseListTest.java
+++ src/test/java/seedu/planpal/expenses/ViewExpenseListTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.planpal.exceptions.PlanPalExceptions;
 import seedu.planpal.modes.expenses.managers.ExpenseManager;
+import seedu.planpal.utility.parser.modeparsers.ExpenseParser;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -15,10 +16,12 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class ViewExpenseListTest {
     private static final ByteArrayOutputStream OUTPUT_STREAM = new ByteArrayOutputStream();
     private ExpenseManager expenseManager;
+    private ExpenseParser expenseParser;
 
     @BeforeEach
     public void setUp() {
         expenseManager = new ExpenseManager();
+        expenseParser = new ExpenseParser(expenseManager);
         System.setOut(new PrintStream(OUTPUT_STREAM));
         OUTPUT_STREAM.reset();
     }
@@ -29,7 +32,7 @@ public class ViewExpenseListTest {
             expenseManager.getBudgetManager().setBudget("1000");
             expenseManager.addExpense("/name:trial1 /cost:200");
             expenseManager.addExpense("/name:trial2 /cost:300");
-            expenseManager.viewExpenseList();
+            expenseParser.processCommand("list");
             double totalCost = expenseManager.getTotalCost(expenseManager.getMonthlyExpenses());
             assertEquals(500.0, totalCost);
             assertTrue(OUTPUT_STREAM.toString().contains("Remaining budget: $500.0"));
